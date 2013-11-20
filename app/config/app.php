bd09a93c978483a232976d9e916f1356390433aa
Tomás Solar Castro <tsolar@gmail.com>
2013-11-19 23:51:52 -0300

    app.config.app: added Confide alias

--- app/config/app.php
+++ app/config/app.php
@@ -162,5 +162,6 @@ return array(
         'URL' => 'Illuminate\Support\Facades\URL',
         'Validator' => 'Illuminate\Support\Facades\Validator',
         'View' => 'Illuminate\Support\Facades\View',
+        'Confide'    => 'Zizaco\Confide\ConfideFacade',
     ),
 );
